ffef9b3704ba9363f9c886f8d81ac9568dc2cd98
daffa tabiano <daffatabianoo@gmail.com>
2024-04-22 14:51:21 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+// dark light mode 
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () =>{
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+}
